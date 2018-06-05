5780f8b2a797911c5ba45f936695cdacb01d5182
aquzohr <kampee.expertly01@gmail.com>
2018-06-06 02:24:04 +0700

    add port

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const client = new Discord.Client();
 var request = require('request');
 var sleep = require('sleep');
 
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 const botconfig = require('./botconfig.json');
 
 var listBoss = [];
