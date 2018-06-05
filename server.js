a2e0ca78bc2e03fd7c873fbb44136118f2b791bb
aquzohr <kampee.expertly01@gmail.com>
2018-06-05 20:25:06 +0700

    make it better

--- server.js
+++ server.js
@@ -1,16 +1,149 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var request = require('request');
+var cheerio = require('cheerio');
+
 const botconfig = require('./botconfig.json');
+
+var listBoss = [];
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  sendMessageDiscord()
+  setInterval(()=>sendMessageDiscord(),1000*60);
+
+
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('message', message => {
+
+  message.channel.fetchMessages({ limit: 2 })
+  .then(messages => {
+    const fetchedMsg = messages.last(); // messages is a collection!)
+    fetchedMsg.delete(1000*58);
+  });
+})
+
+function sendMessageDiscord(){
+  api = 'https://world-boss-timer-bdoth.firebaseio.com/world_boss.json';
+
+  console.log("update timing...")
+
+  request({url: api, json: true}, function(error, response, data){
+      if(!error){
+
+        findBossNextSpawn(data);
+
+        sendBossTimer(listBoss);
+
+
+      }
+  })
+
+};
+
+function sendBossTimer(listBoss){
+
+  var text;
+  for(var i=0;i<listBoss.length;i++){
+    //console.log(listBoss[i].name);
+    if(i==0){
+      text = '<'+listBoss[i].name+'>    ' + bossTimer(listBoss[i].time,listBoss[i].day) + '  ใกล้จะเกิดแล้ว\n'; 
+    }else{
+      text += '<'+listBoss[i].name+'>    ' + bossTimer(listBoss[i].time,listBoss[i].day) + '  รอเกิดต่อไป\n';       
+    }
   }
-});
+
+  client.channels
+  .find("name","world-boss-timers")
+  .send('```'+ text +'```');
+  
+}
+
+function bossTimer(bosstime,bossday){
+
+  //for boss
+  curr_day=new Date().getDay();
+  boss_time = bosstime*60*60;
+  if(bossday > curr_day){
+    boss_time += (bossday-curr_day)*24*60*60;
+  }
+
+  //curent
+  hour=new Date().getHours();
+  min= new Date().getMinutes();
+  sec= new Date().getSeconds();
+
+  current_time = (hour*60*60) + (min*60) + sec;
+
+  return countdown(boss_time-current_time);
+}
+
+const countdown = (function () {
+  const pad = t => {
+      return (t + '').length < 2 ? pad('0' + t + '') : t ;
+  }
+  return s => {
+
+      const d = Math.floor(s / (3600 * 24));
+
+      s  -= d * 3600 * 24;
+
+      const h   = Math.floor(s / 3600);
+
+      s  -= h * 3600;
+
+      const m = Math.floor(s / 60);
+
+      s  -= m * 60;
+
+      const tmp = [];
+
+      (d) && tmp.push(d + 'd');
+
+      (d || h) && tmp.push(h + 'h');
+
+      (d || h || m) && tmp.push(m + 'm');
+
+      //tmp.push(s + 's');
+
+      return tmp.join('');
+  }
+}());
+
+function findBossNextSpawn(data){
+  curr_day=new Date().getDay();
+  hour=new Date().getHours();
+
+  for(var i=0;i<5;i++){
+    for (var key in data) {
+      if(data[key].day==curr_day+i){
+        if(i==0 && hour < data[key].time){
+          listBoss.push({
+            name: data[key].name,
+            time: data[key].time,
+            day: data[key].day
+          });
+        }
+        
+        if(i!=0){
+          listBoss.push({
+            name: data[key].name,
+            time: data[key].time,
+            day: data[key].day
+          });
+        }
+      }
+
+      if(listBoss.length > 4){
+        break;
+      }
+    }
+  }
+
+};
+
 
 client.login(botconfig.token);
